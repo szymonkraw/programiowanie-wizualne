1e481409cc9c325a70eddb72e64b5fd0de95c958
szymonkraw <61711041+szymonkraw@users.noreply.github.com>
2021-10-11 23:42:12 +0200

    Add files via upload

---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			var czas = new System.Windows.Threading.DispatcherTimer();
+			czas.Tick += new EventHandler(ustaw_zegar);
+			czas.Interval = new TimeSpan(0, 0, 1);
+			czas.Start(); 
+		}
+		private void ustaw_zegar(object sender, EventArgs e)
+		{
+			
+			zegar.Content = DateTime.Now.Hour;
+			zegar2.Content = DateTime.Now.Minute; 
+			
+			CommandManager.InvalidateRequerySuggested();
+		}
+
+
+		private void open(object sender, RoutedEventArgs e)
+		{
+			Window1 drugie_okno = new Window1();
+			//this.Visibility = Visibility.Hidden;
+			drugie_okno.Show();
+		}
+		
+	}
+}
---
+++ WpfApp2/WpfApp2/Window1.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading; 
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+       
+        DateTime poczatek;
+        TimeSpan ts;
+        public Window1()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private void start_button(object sender, RoutedEventArgs e)
+        {
+            string test;
+            ts = DateTime.Now - poczatek;
+            wynik.Content = ts.ToString();
+        }
+
+        private void stop_button(object sender, RoutedEventArgs e)
+        {
+            poczatek = new DateTime(DateTime.Now.Ticks);
+          //  Timer1.enabled = true;
+        }
+    }
+}
